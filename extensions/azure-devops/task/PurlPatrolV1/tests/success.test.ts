fb0ea36d9d4e441096e4bb28afa75a944ba458fa
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2025-03-21 16:04:44 +0100

    refactor: should have ran test added

--- extensions/azure-devops/task/PurlPatrolV1/tests/success.test.ts
+++ extensions/azure-devops/task/PurlPatrolV1/tests/success.test.ts
@@ -1,24 +1,45 @@
 import * as assert from 'assert';
 import * as path from 'path';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
-
-import {describe, it} from 'node:test';
+import {describe, it, before} from 'node:test';
 
 
 const dirname = "./dist/helpers/"
 describe('loading the pipeline task successfully with only required inputs', function () {
     let tr: ttm.MockTestRunner;
-    let tp = path.join(dirname, 'success.js');
-    tr = new ttm.MockTestRunner(tp); 
-
-    it("should run successfully", {timeout:5000}, async() => {
+    
+    before(async function() {
+        let tp = path.join(dirname, 'success.js');
+        tr = new ttm.MockTestRunner(tp);
         await tr.runAsync();
-        assert.equal(tr.succeeded, true, 'The task should have succeeded');
-        assert.equal(tr.stdout.includes("Docker ran successfully"), true, 'Should have contained the string "Docker ran successfully' )
-        assert.equal(tr.errorIssues.length, 0, "should have no errors");
-        assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-        assert.equal(tr.stdout.indexOf('Running PURL Patrol...') >= 0, true, "should display Running PURL Patrol...");
-        assert.equal(tr.stdout.indexOf('PURL Patrol completed successfully') >= 0, true, "should display PURL Patrol completed successfully");
+    });
+    
+    it("should have ran", function() {
+        assert.ok(tr.ran)
+    });
+    
+    it("should succeed", function() {
+        assert.equal(tr.succeeded, true);
+    });
+    
+    it("should show Docker ran successfully", function() {
+        assert.equal(tr.stdout.includes("Docker ran successfully"), true);
+    });
+    
+    it("should have no errors", function() {
+        assert.deepEqual(tr.errorIssues, []);
+    });
+    
+    it("should have no warnings", function() {
+        assert.deepEqual(tr.warningIssues, []);
+    });
+    
+    it("should display 'Running PURL Patrol...'", function() {
+        assert.match(tr.stdout, /Running PURL Patrol/);
+    });
+    
+    it("should display 'PURL Patrol completed successfully'", function() {
+        assert.match(tr.stdout, /PURL Patrol completed successfully/);
     });
 });
 
