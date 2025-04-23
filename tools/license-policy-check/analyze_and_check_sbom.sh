031b04cb14400c8fb1c20271ad3cc3ddea58498d
thanhngan.tran <thanhngan.tran@maibornwolff.de>
2025-04-23 13:40:10 +0200

    fix: changed flag

--- tools/license-policy-check/analyze_and_check_sbom.sh
+++ tools/license-policy-check/analyze_and_check_sbom.sh
@@ -3,10 +3,11 @@
 
 LICENSE_POLICY_PATH="${LICENSE_POLICY_PATH:-/sbom/mw_license_policy.json}"
 BREAK_ENABLED="${BREAK_ENABLED:-true}"
-IGNORE_PKG_TYPES="${IGNORE_PKG_TYPES:-""}"
+IGNORE_PKG_TYPES="${IGNORE_PKG_TYPES:-''}"
 
 if [ -z "$SBOM_PATH" ] || [ -z "$LICENSE_POLICY_PATH" ] || [ -z "$BREAK_ENABLED" ] || [ -z "$IGNORE_PKG_TYPES" ]; then
-  echo "Missing variables in check script. Please check SBOM_PATH or LICENSE_POLICY_PATH or BREAK_ENABLED"
+  echo "Missing variables in check script. Please check SBOM_PATH or LICENSE_POLICY_PATH or BREAK_ENABLED or IGNORE_PKG_TYPES"
+  echo "$IGNORE_PKG_TYPES"
   exit 1
 fi
 /sbom/sbom-utility license list --input-file="$SBOM_PATH" --format=csv --config-license="$LICENSE_POLICY_PATH" -o="evaluated_sbom.csv" --quiet
